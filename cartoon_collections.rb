6398ca5225ac706bab1b718d4809f89b5942a556
mcksarge <higgins.fred.s@gmail.com>
2022-04-20 10:02:37 -0700

    commit tree fa99dcd993a84c3481dbb01f6246a55a82cdc1e3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index| puts "#{index + 1}. #{dwarf}" #Iterates through array, and prints their name and index number
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  elements = []
+  array.map{|element| elements.push("#{element.capitalize}!")}
+  elements
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  result = false
+  array.each do |word|
+    if word.length > 4
+      result = true
+    end
+  end
+  result
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|item| cheese_types.include?(item)}
 end
